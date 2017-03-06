89bf28b694133aefcb850622208e6563cc959122
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2017-03-06 17:04:56 +0100

    working on

---
+++ src/model/common.py
@@ -0,0 +1,10 @@
+"""
+@package p2psp_simulator
+common module
+"""
+
+from core.color import Color
+
+class Common:
+
+    MAX_CHUNK_NUMBER = 65536
--- src/model/peer_core.py
+++ src/model/peer_core.py
@@ -3,11 +3,14 @@
 peer_core module
 """
 from queue import Queue
+from common import Common
 
 class Peer_core():
     def __init__(self):
         self.socket = Queue()
-        self.alive = True
+        self.played_chunk = -1
+        self.prev_received_chunk = 0
+        self.player_alive = True
         print("DBS initialized")
         
     def process_message(self, message, sender):
@@ -28,12 +31,16 @@ class Peer_core():
         play_next_chunks(last_received_chunk)
 
     def play_next_chunks(self, last_received_chunk):
-        for i in range(last_received_chunk-prev_received_chunk):
-            play_chunk(played_chunk)
+        for i in range(last_received_chunk - self.prev_received_chunk):
+           self.player_alive = play_chunk(self.played_chunk)
+           #LOG chunks consumed and lost chunks
+           self.played_chunk = (self.played_chunk + 1) % MAX_CHUNK_NUMBER
+           if ((self.prev_received_chunk % MAX_CHUNK_NUMBER) < last_received_chunk):
+               self.prev_received_chunk = last_received_chunk
         
     def play_chunk(self, chunk_number):
         raise NotImplementedError
 
     def run(self):
-        while(alive):
+        while(self.player_alive):
             keep_the_buffer_full()
