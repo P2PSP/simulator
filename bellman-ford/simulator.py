d20102929a5e32bd86e9db9677d03c8303d0e780
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2017-03-08 13:20:15 +0100

    .

--- bellman-ford/simulator.py
+++ bellman-ford/simulator.py
@@ -1,26 +1,34 @@
 import threading
 import queue
 
-team = [] # List of nodes
+queues = [queue.Queue(10)] * 10
 
 class Node(threading.Thread):
 
-    q = queue.Queue(10)
-    distances = []       # Distance to each node
+    distances = [1000]*10       # Distance to each node
     
-    def __init__(self, iters=10):
+    def __init__(self, node):
         super(Node,self).__init__()
+        self.node = node
+        Node.distances[self.node] = 0
 
-    def define_distance_to_none(self, node, distance):
-        distances[node] = distance
+#    def define_distance_to_node(self, node, distance):
+#        distances[node] = distance
 
     # Runs Bellman-Ford algorithm for routing between nodes
     def run(self):
-        while (received_distances, neighbour_node) = q.get():
+        while True:
+
+            # Compute distances
+            received_distances, neighbour_node = Node.q.get()
             for i in enum(received_distances):
                 if received_distances[i].distance + distances[neighbour_node] < distances[i]:
                     gateway[i] = neighbour_node
-                    distance[i] = received_distances[i].distance + distances[neighbour_node]
+                    distances[i] = received_distances[i].distance + distances[neighbour_node]
 
+            # Communicate distances
+            for i in len(distances):
+                queue[i].put(Node.distances)
+                    
             for i in distances:
                 print("({},{}) ",format(i, distances[i]))
