a57a5f6a2c9398ce3374a7617fd98687ad60a2ac
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2017-03-13 11:42:11 +0100

    .

--- bellman-ford/simulator.py
+++ bellman-ford/simulator.py
@@ -1,6 +1,6 @@
-import multiprocessing
-#import threading
-#import queue
+#import multiprocessing
+import threading
+import queue
 import sys
 import io
 
@@ -10,24 +10,25 @@ queues = [None]*number_of_nodes
 
 class Node():
 
-    distances = [1000]*number_of_nodes # Distance to each node
+    #distances = [1000]*number_of_nodes # Distance to each node
     
     def __init__(self, node):
         super(Node,self).__init__()
         self.node = node # Node number
         self.gateways = []
-        queues[self.node] = multiprocessing.Queue(10)
-        #queues[self.node] = queue.Queue(10)
-        Node.distances[self.node] = 0
+        self.distances = [1000]*number_of_nodes # Distance to each node
+        #queues[self.node] = multiprocessing.Queue(10)
+        queues[self.node] = queue.Queue(10)
+        self.distances[self.node] = 0
 
     def set_distance(self, node, distance):
-        Node.distances[node] = distance
+        self.distances[node] = distance
         if not node in self.gateways:
             self.gateways.append(node)
-        print('Node', self.node, 'distances =', Node.distances)
+        print('Node', self.node, 'distances =', self.distances)
 
     def get_distances(self):
-        return Node.distances
+        return self.distances
 
     # Runs Bellman-Ford algorithm for routing between nodes
     def run(self):
@@ -37,13 +38,16 @@ class Node():
             found_new_route = False
 
             # Compute distances
-            print('Node', self.node, ': current distances =', Node.distances)
+            print('Node', self.node, ': current distances =', self.distances)
             received_distances, neighbour_node = queues[self.node].get()
-            print('Node', self.node, ': Received', received_distances, 'from node', neighbour_node)
+            print('Node', self.node, ': Received', received_distances, \
+                  'from node', neighbour_node)
             for i,distance in enumerate(received_distances):
-                print('distance=', distance, 'Node.distances[', neighbour_node, ']=', Node.distances[neighbour_node], 'Node.distances[',i,']=',Node.distances[i])
-                if distance + Node.distances[neighbour_node] < Node.distances[i]:
-                    Node.distances[i] = distance + Node.distances[neighbour_node]
+                print('distance=', distance, 'self.distances[', neighbour_node, \
+                      ']=', self.distances[neighbour_node], \
+                      'self.distances[',i,']=',self.distances[i])
+                if distance + self.distances[neighbour_node] < self.distances[i]:
+                    self.distances[i] = distance + self.distances[neighbour_node]
                     found_new_route = True
                     print('Found new route!')
                     
@@ -54,16 +58,16 @@ class Node():
                 print('Node', self.node, 'Transmiting vector of distances')
                 for gw in self.gateways:
                     print("gw =",gw)
-                    print("distances =", Node.distances)
-                    queues[gw].put((Node.distances, self.node))
+                    print("distances =", self.distances)
+                    queues[gw].put((self.distances, self.node))
 
-            for i,distance in enumerate(Node.distances):
+            for i,distance in enumerate(self.distances):
                 print("({},{})".format(i, distance), end=' ')
             print()
 
             sys.stdout.flush()
 
     def start(self):
-        #threading.Thread(target=self.run).start()
-        multiprocessing.Process(target=self.run).start()
+        threading.Thread(target=self.run).start()
+        #multiprocessing.Process(target=self.run).start()
         
