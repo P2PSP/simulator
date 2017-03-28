6d287a90f01fcc6f9a2cf96329f0e65e822f30d3
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2017-03-28 11:17:22 +0200

    working on CIS

--- src/core/splitter_strpeds.py
+++ src/core/splitter_strpeds.py
@@ -14,33 +14,41 @@ class Splitter_STRPEDS(Splitter_DBS):
     
     def __init__(self):
         super().__init__()
+        self.trusted_peers = []
+        self.bad_peers = []
         print("Splitter STRPEDS initialized")
         
 
     def send_dsa_key(self):
-        #is it needed?
+        #it is not needed in terms of simulation
         return NotImplementedError
 
     def gather_bad_peers(self):
-
-    def get_peer_for_gathering(self):
-
-    def get_trusted_peer_for_gathering(self):
-
-    def request_bad_peers(self, dest):
+        for p in self.peer_list:
+            Common.UDP_SOCKETS[p].put(self.id,(-1,"S"))
 
     def init_key(self):
+        #it is not needed in terms of simulation
         return NotImplementedError
 
-    def get_message(self, chunk_number, chunk, dst):
-
-    def add_trusted_peer(self, peer):
-
-    def process_bad_peers_message(self,message, sender):
+    def process_bad_peers_message(self, message, sender):
+        bad_list = message[1]
+        for bad_peer in bad_list:
+            if sender in self.trusted_peers:
+                self.handle_bad_peer_from_trusted(bad_peer, sender)
+            else:
+                self.handle_bad_peer_from_regular(bad_peer, sender)
 
     def handle_bad_peer_from_trusted(self, bad_peer, sender):
+        self.add_complain(bad_peer, sender)
+        if bad_peer not in self.bad_peers:
+            self.bad_peers.append(bad_peer)
 
     def handle_bad_peer_from_regular(self, bad_peer, sender):
+        self.add_complain(bad_peer, sender)
+        x = len(self.complains[bad_peer])/len(self.peer_list)
+        if x >= self.majority_ratio:
+            self.punish_peer(bad_peer)
 
     def punish_peer(self, peer, message):
 
