70181e33688590e5bbd9e66e87833726f9400540
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2017-04-06 11:45:43 +0200

    .

--- experiments/dbs2-1-2-3/run.py
+++ experiments/dbs2-1-2-3/run.py
@@ -2,25 +2,28 @@ import simulator
 import time
 
 n0 = simulator.Node(0)
-n1 = simulator.Node(1)
-n2 = simulator.Node(2)
-
-# Set absolute distances statically
-n0.set_distance(1,1)
-n0.set_distance(2,2)
-n1.set_distance(2,1)
-n1.set_distance(0,1)
-n2.set_distance(0,2)
-n2.set_distance(1,1)
-
-print(n0.get_distances())
-print(n1.get_distances())
-print(n2.get_distances())
-
 n0.start()
-n1.start()
-n2.start()
 
 simulator.queues[0].put((0, -1)) # Peer, chunk, splitter
+simulator.queues[0].put((1, -1)) # Peer, chunk, splitter
+
+n1 = simulator.Node(1)
+n0.add_neighbor(1)
+n1.add_neighbor(0)
+n1.start()
+
+simulator.queues[0].put((2, -1)) # Peer, chunk, splitter
+simulator.queues[0].put((3, -1)) # Peer, chunk, splitter
+
+n2 = simulator.Node(2)
+n2.add_neighbor(1)
+n1.add_neighbor(2)
+print(n0.get_neighbors())
+print(n1.get_neighbors())
+print(n2.get_neighbors())
+n2.start()
+
+simulator.queues[0].put((4, -1)) # Peer, chunk, splitter
+simulator.queues[0].put((5, -1)) # Peer, chunk, splitter
 
 
--- experiments/dbs2-1-2-3/simulator.py
+++ experiments/dbs2-1-2-3/simulator.py
@@ -2,6 +2,7 @@ import threading
 import queue
 import sys
 import io
+import time
 
 number_of_nodes = 3
 buffer_size = number_of_nodes
@@ -13,54 +14,53 @@ class Node():
     def __init__(self, node):
         super(Node,self).__init__()
         self.node = node # Node number
-        self.gateways = []
-        self.distances = [1000]*number_of_nodes # Distance to each node
+        self.neighbors = []
+        self.buffer = [None] * buffer_size
         queues[self.node] = queue.Queue(10)
-        self.distances[self.node] = 0
 
-    def set_distance(self, node, distance):
-        self.distances[node] = distance
-        if not node in self.gateways:
-            self.gateways.append(node)
-        print('Node', self.node, ': distances =', self.distances)
+    def add_neighbor(self, node):
+        self.neighbors.append(node)
 
-    def get_distances(self):
-        return self.distances
+    def get_neighbors(self):
+        return self.neighbors
 
     # Run forwarding algorithm
     def run(self):
 
         print('Node', self.node, ': running')
 
-        # Simulate the reception of the neighbors_list
-        neighbors_list = []
-        for node, distance in enumerate(self.distances):
-            if distance == 1:
-                neighbors_list.append(node)
-        print('Node', self.node, ':', neighbors_list)
-
-        # Create the buffer
-        _buffer = [None] * buffer_size
-
         neighbors_counter = 0
         while True:
-            # Receive a chunk from node neighbor
-            chunk, neighbor = queues[self.node].get()
-            print('Node', self.node, ': received chunk', chunk, 'from', neighbor)
+            # Receive a chunk
+            chunk, sender = queues[self.node].get()
+            print('Node', self.node, ': received chunk', chunk, 'from', sender)
             
             # Store the chunk in the buffer
-            _buffer[chunk % buffer_size] = chunk
+            self.buffer[chunk % buffer_size] = chunk
 
-            # Send the chunk
-            destination_node = neighbors_counter % len(neighbors_list)
-            queues[destination_node].put((chunk, self.node))
-            print('Node', self.node, 'neighbors_counter =', neighbors_counter, \
-                  'neighbors_list =', neighbors_list, \
-                  'destination =', destination_node, \
-                  'chunk =', chunk)
-            neighbors_counter = ( neighbors_counter + 1 ) % len(neighbors_list)
+            if len(self.neighbors) > 0:
+                # Determine a destination
+                destination_node = self.neighbors[neighbors_counter % len(self.neighbors)]
+                while destination_node == sender:
+                    neighbors_counter = ( neighbors_counter + 1 ) % len(self.neighbors)
+                    destination_node = self.neighbors[neighbors_counter % len(self.neighbors)]
+                    if destination_node == sender:
+                        break
+                    print(self.node, end='')
 
+                if destination_node != sender:
+                    # Send the chunk
+                    queues[destination_node].put((chunk, self.node))
+                    print('Node', self.node, ': sent chunk', chunk, 'towards', destination_node)
+                
+            if __debug__:
+                print('Node', self.node, 'neighbors_counter =', neighbors_counter, \
+                      'neighbors =', self.neighbors, \
+                      'destination =', destination_node, \
+                      'chunk =', chunk)
+            
             sys.stdout.flush()
+            time.sleep(0.1)
 
     def start(self):
         threading.Thread(target=self.run).start()
