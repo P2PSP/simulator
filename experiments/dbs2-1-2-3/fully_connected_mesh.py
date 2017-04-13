347aede4976ce5d5ded2bc3aeccd21377ba0115b
vicente-gonzalez-ruiz <vicente.gonzalez.ruiz@gmail.com>
2017-04-13 11:23:15 +0200

    .

--- experiments/dbs2-1-2-3/fully_connected_mesh.py
+++ experiments/dbs2-1-2-3/fully_connected_mesh.py
@@ -6,6 +6,7 @@ import queue
 import sys
 import io
 import time
+#import colorama
 
 max_number_of_nodes = 3
 buffer_size = 40
@@ -21,7 +22,8 @@ class Node():
         self.buffer = [None] * buffer_size
         self.sender = [None] * buffer_size
         queues[self.node] = queue.Queue()
-        self.splitter_chunks = []
+        self.chunks_to_relay = []
+        #colorama.init()
         
     # Run forwarding algorithm
     def run(self):
@@ -44,15 +46,13 @@ class Node():
             self.sender[chunk % buffer_size] = sender
 
             # Print the content of the buffer
-            print('Node {}: buffer = |'.format(self.node), end='')
+            print('Node {}: buffer = '.format(self.node), end='')
             for i in self.buffer:
                 if i != None:
-                    if self.sender[i] == -1:
-                        print('{:2d}*'.format(i), end='')
-                    else:
-                        print('{:2d}.'.format(i), end='')
+                    #print(colorama.Fore.RED + '{:2d}*'.format(i), end=colorama.Style.RESET_ALL + '')
+                    print('{:2d},{:2d} '.format(i, self.sender[i]), end='')
                 else:
-                    print('  |', end='')
+                    print('  ,   ', end='')
             print()
             
             # Flooding pattern: send the chunk received from the
@@ -60,35 +60,38 @@ class Node():
 
             # Each chunk should follow its own run along the list of peers.
             
-            splitter_chunk = None
-            
             if sender == -1:
 
                 # A new chunk has arrived from the splitter.
-                self.splitter_chunks.append((chunk, 0)) # (chunk, first peer to send it)
+                self.chunks_to_relay.append((chunk, self.node)) # (chunk, first peer to send it)
 
-            c = 0
-            for i in self.splitter_chunks:
+            #c = 0
+            for c, i in enumerate(self.chunks_to_relay):
 
-                destination_node = self.splitter_chunks[c][1]
-                while destination_node == self.node:
-                    destination_node = (destination_node + 1) % Node.number_of_nodes
+                relayed_chunk, destination_node = i
+                if __debug__:
+                    print('Node {}: i={} chunks_to_relay={}'.format(self.node, i, self.chunks_to_relay))
                     
-                if destination_node == 0:
-                    del(self.splitter_chunks[c])
-                    c += 1
-                    continue
-                else:
-                    queues[destination_node].put((splitter_chunk, self.node))
-                    if __debug__:
-                        print('Node {}: sent {} to {} (number_of_nodes={})'.\
-                              format(self.node, chunk, destination_node, Node.number_of_nodes))
-
                 destination_node = (destination_node + 1) % Node.number_of_nodes
-                
-                self.splitter_chunks[c] = (self.splitter_chunks[c][0], destination_node)
+                #while destination_node == self.node:
+                #    destination_node = (destination_node + 1) % Node.number_of_nodes
 
-                c += 1                    
+                if destination_node == self.node:
+                #if c>= (Node.number_of_nodes - 1):    
+                #if destination_node == 0:
+                    if __debug__:
+                        print('Node {}: deleted {}'.format(self.node, self.chunks_to_relay[c]))
+                    del(self.chunks_to_relay[c])
+                    continue
+
+                queues[destination_node].put((relayed_chunk, self.node))
+                if __debug__:
+                    print('Node {}: sent {} to {} (number_of_nodes={})'.\
+                          format(self.node, relayed_chunk, destination_node, Node.number_of_nodes))
+                
+                self.chunks_to_relay[c] = (relayed_chunk, destination_node)
+
+                #c += 1                    
             
             sys.stdout.flush()
             time.sleep(0.1)
