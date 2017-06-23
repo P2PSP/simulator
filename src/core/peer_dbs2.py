2e7c53eccf1dc45425269a36d5864976062ccb73
Vicente González Ruiz <vruiz@ual.es>
2017-06-23 10:44:16 +0200

    .

--- src/core/peer_dbs2.py
+++ src/core/peer_dbs2.py
@@ -2,17 +2,31 @@
 @package simulator
 peer_dbs module
 """
+
 from queue import Queue
 from threading import Thread
 from .common import Common
-from .peer_core import Peer_core
+#from .peer_core import Peer_core
 import time
 
-class Peer_DBS(Peer_core):
+class Peer_DBS():
     MAX_CHUNK_DEBT = 128
     
-    def __init__(self,id):
-        super().__init__(id)
+    def __init__(self, id):
+        self.id = id
+        self.socket = Common.UDP_SOCKETS[self.id]
+        self.played_chunk = 0
+        self.prev_received_chunk = 0
+        self.buffer_size = 64
+        self.player_alive = True
+        self.chunks = []
+
+        #---Only for simulation purposes----
+        self.losses = 0
+        self.played = 0
+        self.number_of_chunks_consumed = 0
+        #----------------------------------
+
         self.max_chunk_debt = self.MAX_CHUNK_DEBT
         self.peer_list = []
         self.debt = {}
@@ -20,14 +34,21 @@ class Peer_DBS(Peer_core):
         self.number_of_monitors = 0
         self.receive_and_feed_counter = 0
         self.receive_and_feed_previous = ()
-        #self.debt_memory = 0
+        self.debt_memory = 0
         self.waiting_for_goodbye = False
         self.modified_list = False
         self.number_of_peers = 0
         self.sendto_counter = 0
         self.ready_to_leave_the_team = False        
         print("max_chunk_debt", self.MAX_CHUNK_DEBT)
+        print("Peer", self.id)
         print("Peer DBS initialized")
+
+    def set_splitter(self, splitter):
+        self.splitter = {}
+        self.splitter["id"] = splitter
+        self.splitter["socketTCP"] = Common.TCP_SOCKETS[splitter]
+        self.splitter["socketUDP"] = Common.UDP_SOCKETS[splitter]
 
     def say_hello(self, peer):
         hello = (-1,"H")
@@ -63,14 +84,34 @@ class Peer_DBS(Peer_core):
         print("list of peers received. Size",len(self.peer_list))
 
     def connect_to_the_splitter(self):
-        Peer_core.connect_to_the_splitter(self)
+        hello = (-1,"P")
+        self.splitter["socketTCP"].put((self.id, hello))
+
+    def send_ready_for_receiving_chunks(self):
+        ready = (-1, "R")
+        self.splitter["socketTCP"].put((self.id, ready))
 
     def send_chunk(self, peer):
         Common.UDP_SOCKETS[peer].put((self.id,self.receive_and_feed_previous))
         self.sendto_counter += 1
 
+    def is_a_control_message(self, message):
+        if message[0] == -1:
+            return True
+        else:
+            return False
+    
     def process_message(self, message, sender):
-        Peer_core.process_message(self, message, sender)
+        # ----- Only for simulation purposes ------
+        # ----- Check if new round for peer -------
+        if not self.is_a_control_message(message) and sender == self.splitter["id"]:
+            if self.played > 0 and self.played >= len(self.peer_list):
+                clr = self.losses/self.played
+                Common.SIMULATOR_FEEDBACK["DRAW"].put(("CLR",self.id,clr))
+                self.losses = 0
+                self.played = 0
+        # ------------------------------------------
+        
         if (message[0] >= 0):
             chunk_number = message[0]
             chunk = message[1]
@@ -187,6 +228,10 @@ class Peer_DBS(Peer_core):
                         self.waiting_for_goodbye = False
             return -1
 
+    def process_next_message(self):
+        content = self.socket.get() #it replaces receive_next_message
+        return self.process_message(content[1], content[0])
+        
     def polite_farewell(self):
         print("Goodbye!")
         while (self.receive_and_feed_counter < len(self.peer_list)):
@@ -206,10 +251,52 @@ class Peer_DBS(Peer_core):
         self.receive_and_feed_counter = 0
         self.receive_and_feed_previous = ()
         self.sendto_counter = 0
-        #self.debt_memory = 1 << self.MAX_CHUNK_DEBT
+        self.debt_memory = 1 << self.MAX_CHUNK_DEBT
         self.waiting_for_goodbye = True
-        Peer_core.buffer_data(self)
+        for i in range(self.buffer_size):
+            self.chunks.append((i,"L"))
+        
+        chunk_number = self.process_next_message()
+        
+        while(chunk_number < 0):
+            chunk_number = self.process_next_message()
 
+        self.played_chunk = chunk_number
+
+        print(self.id,"Position in the buffer of the first chunk to play", str(self.played_chunk))
+        
+        while (chunk_number < self.played_chunk or ((chunk_number - self.played_chunk) % self.buffer_size) < (self.buffer_size // 2)):
+            chunk_number = self.process_next_message()
+            #while (chunk_number < 0 or chunk_number < self.played_chunk):
+            while (chunk_number < self.played_chunk):
+                chunk_number = self.process_next_message()
+                    
+        self.prev_received_chunk = chunk_number
+
+    def play_next_chunks(self, last_received_chunk):
+        for i in range(last_received_chunk - self.prev_received_chunk):
+            self.player_alive = self.play_chunk(self.played_chunk)
+            self.chunks[self.played_chunk % self.buffer_size] = (self.played_chunk,"L")
+            self.played_chunk = (self.played_chunk + 1) % Common.MAX_CHUNK_NUMBER
+        if ((self.prev_received_chunk % Common.MAX_CHUNK_NUMBER) < last_received_chunk):
+            self.prev_received_chunk = last_received_chunk
+    
+    def play_chunk(self, chunk_number):
+        if self.chunks[chunk_number%self.buffer_size][1] == "C":
+            self.played +=1
+        else:
+            self.losses += 1
+            print (self.id, "Lost Chunk!", chunk_number)
+        self.number_of_chunks_consumed += 1
+        return self.player_alive
+
+    def keep_the_buffer_full(self):
+        last_received_chunk = self.process_next_message()
+        while (last_received_chunk < 0):
+            last_received_chunk = self.process_next_message()
+
+        self.play_next_chunks(last_received_chunk)
+    
     def start(self):
         Thread(target=self.run).start()
         
