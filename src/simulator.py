a4efad8f3261021f9c58d2eb114bf8749bf59917
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2017-07-07 20:52:52 +0200

    run finished message removed

--- src/simulator.py
+++ src/simulator.py
@@ -416,7 +416,6 @@ class Simulator():
 
                     Socket_queue.UDP_SOCKETS['S'].put(((-1, "K"), "SIM"))
             m= queue.get()
-        print("Run finished!")
 
     def addPeer(self):
         probabilities = [Simulator.P_MoP, Simulator.P_WIP, Simulator.P_MP]
