a3477d1c046cd5b3112bbbdc1ce7f6f503f0f7a9
Juan Pedro <jotaperamez@gmail.com>
2018-01-11 09:08:47 -0800

    Facilita el arranque del simulador

---
+++ launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -f *udp *tcp #removal trash files
+exe="python3 -u src/simulator.py run --set_of_rules $1 --number-of-monitors $2 --number-of-peers $3 --number-of-rounds $4 --drawing-log $HOME/$5"
+
+if [ $6 -eq 1 ]
+then
+    $exe --gui
+else
+    $exe
+fi
