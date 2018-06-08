be1dc7b2c639a68c48abcb50703ae0fc91911fef
Sachin <sachinsngh165@gmail.com>
2018-06-08 16:52:35 +0530

    files related to cis set of rules changed as per new DBS rules and internet socket

--- src/core/monitor_strpeds.py
+++ src/core/monitor_strpeds.py
@@ -4,28 +4,35 @@ monitor_strpeds module
 """
 
 from .peer_strpeds import Peer_STRPEDS
-
+from .simulator_stuff import Simulator_socket as socket
+from .common import Common
+import struct
 
 class Monitor_STRPEDS(Peer_STRPEDS):
     def __init__(self, id):
+        self.losses = 0
         super().__init__(id)
         print("STRPEDS initialized by monitor")
 
     def receive_buffer_size(self):
-        self.buffer_size = self.splitter_socket.recv("H")
-        print(self.id, ": received buffer_size =", self.buffer_size, "from S")
+
+        Peer_STRPEDS.receive_buffer_size(self)
         self.buffer_size //= 2
 
         # --- Only for simulation purposes ----
         self.sender_of_chunks = [""] * self.buffer_size
         # -------------------------------------
 
-    def complain(self, chunk_position):
-        lost = (chunk_position, "L", self.id)
-        self.team_socket.sendto("is6s", lost, self.splitter)
-        print(self.id, ": lost chunk =", lost, "sent to", self.splitter)
+    def complain(self, chunk_number):
+        msg = struct.pack("ii", Common.REQUEST, chunk_number)
+        self.team_socket.sendto(msg, self.splitter)
+        self.lg.info("{}: [request {}] sent to {}".format(self.id, chunk_number, self.splitter))
 
-    def play_chunk(self, chunk_number):
+    def request_chunk(self, chunk_number, peer):
+        Peer_STRPEDS.request_chunk(self, chunk_number, peer)
+        self.complain(chunk_number)
+
+'''    def play_chunk(self, chunk_number):
         if self.chunks[chunk_number % self.buffer_size][1] == "C":
             self.played += 1
         else:
@@ -34,3 +41,4 @@ class Monitor_STRPEDS(Peer_STRPEDS):
             self.complain(chunk_number)
         self.number_of_chunks_consumed += 1
         return self.player_alive
+    '''
--- src/core/peer_strpeds.py
+++ src/core/peer_strpeds.py
@@ -2,8 +2,12 @@
 @package simulator
 peer_strpeds module
 """
+from .common import Common
 from .simulator_stuff import Simulator_stuff as sim
+from .simulator_stuff import Simulator_socket as socket
 from .peer_dbs import Peer_DBS
+import struct
+
 
 
 class Peer_STRPEDS(Peer_DBS):
@@ -16,33 +20,16 @@ class Peer_STRPEDS(Peer_DBS):
         # Not needed for simulation
         return NotImplementedError
 
-    def say_hello(self, peer):
-        hello = (-1, "H", -1)
-        self.team_socket.sendto("isi", hello, peer)
-
-    def say_goodbye(self, peer):
-        if peer == self.splitter:
-            goodbye = (-1, "G", self.id)
-            self.team_socket.sendto("is6s", goodbye, peer)
-        else:
-            goodbye = (-1, "G", -1)
-            self.team_socket.sendto("isi", goodbye, peer)
-
     def process_bad_message(self, message, sender):
         print(self.id, "adding", sender, "to bad list", message)
         self.bad_peers.append(sender)
-        if sender in self.peer_list:
-            self.peer_list.remove(sender)
+        if self.id in self.forward and sender in self.forward[self.id]:
+            self.forward[self.id].remove(sender)
         sim.FEEDBACK["DRAW"].put(("O", "Edge", "OUT", self.id, sender))
 
     def check_message(self, message, sender):
-        if sender in self.bad_peers:
-            if __debug__:
-                print(self.id, "Sender is in bad peer list:", sender)
-            return False
-
         if not self.is_a_control_message(message):
-            if message[1] == "C":
+            if message[1] == b"C":
                 return True
             else:  # (L)ost or (B)roken
                 return False
@@ -53,27 +40,22 @@ class Peer_STRPEDS(Peer_DBS):
 
     def handle_bad_peers_request(self):
         for b in self.bad_peers:
-            self.team_socket.sendto("is6s", (-1, "S", b), self.splitter)
+            bad_msg = (-1, b'S', socket.ip2int(b[0]),b[1])
+            msg = struct.pack('isli',*bad_msg)
+            self.team_socket.sendto(msg, self.splitter)
         if __debug__:
             print(self.id, "Bad peers sent to the Splitter", self.bad_peers)
-        return -1
-
-    def send_chunk(self, peer):
-        self.team_socket.sendto("isi", self.receive_and_feed_previous, peer)
-        self.sendto_counter += 1
-
-    def process_next_message(self):
-        message, sender = self.team_socket.recvfrom("isi")
-        return self.process_message(message, sender)
+        return (-1,-1)
+        
 
     def process_message(self, message, sender):
         if sender in self.bad_peers:
             if __debug__:
                 print(self.id, "Sender is  in the bad peer list", sender)
-            return -1
+            return (-1,-1)
 
-        if sender == self.splitter or self.check_message(message, sender):
-            if self.is_a_control_message(message) and message[1] == "S":
+        if self.check_message(message, sender):
+            if self.is_a_control_message(message) and len(message)>1 and message[1] == 'S':
                 return self.handle_bad_peers_request()
             else:
                 return Peer_DBS.process_message(self, message, sender)
@@ -81,4 +63,4 @@ class Peer_STRPEDS(Peer_DBS):
             self.process_bad_message(message, sender)
             return self.handle_bad_peers_request()
 
-        return -1
+        return (-1,-1)
