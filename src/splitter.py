2f8704a1138d5834f6105672b42d273045ffcfc7
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2018-09-26 11:58:10 +0200

    log parameter added to splitter

--- src/splitter.py
+++ src/splitter.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
                         help="Buffer size")
     parser.add_argument("-p", "--port", type=int,
                         help="Splitter port")
+    parser.add_argument("--log", default=logging.ERROR, help="Log level")
     args = parser.parse_args()
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -20,7 +21,9 @@ if __name__ == "__main__":
         splitter = Splitter_DBS()
     # elif self.set_of_rules == "ims":
         # splitter = Splitter_IMS()
-
+    lg = logging.getLogger("Splitter_DBS")
+    lg.setLevel(args.log)
+    
     splitter.setup_peer_connection_socket(args.port)
     splitter.setup_team_socket()
     splitter_address = splitter.get_id()
