aeb6cd8919dc84cecef5841627ed42ff5d7490df
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-09-13 12:56:23 +0200

    .

--- src/core/splitter_dbs_simulator.py
+++ src/core/splitter_dbs_simulator.py
@@ -27,15 +27,106 @@ class Splitter_DBS_simulator(Simulator_stuff, Splitter_DBS):
                  loglevel = logging.ERROR
     ):
         super().__init__(buffer_size = buffer_size,
-                         max_chunk_loss = max_chunk_loss,
-                         name = name,
-                         loglevel = loglevel
+                         max_chunk_loss = max_chunk_loss
         )
+        self.lg = logging.getLogger(name)
+        self.lg.setLevel(loglevel)
         self.number_of_rounds = number_of_rounds
-        self.lg.debug("Splitter_DBS_simulator: initialized")
+        self.lg.debug("{name}: initialized")
         colorama.init()
         #self.total_lost_chunks = 0
 
+    def setup_peer_connection_socket(self, port=0):
+        super().setup_peer_connection_socket(port = port)
+        self.id = self.peer_connection_socket.getsockname()
+        self.lg.info(f"{self.id}: I am the splitter")
+
+    def send_chunk(self, chunk_number, chunk, peer):
+        super().send_chunk(chunk_number = chunk_number,
+                           chunk = chunk,
+                           peer = peer)
+        self.lg.info(f"{self.id}: chunk {chunk_number} sent to {peer}")
+
+    def handle_arrivals__feedback(self, peer):
+        self.lg.info(f"{self.id}: new connection from incoming {peer}")
+
+    def handle_a_peer_arrival(self, connection):
+        super().handle_a_peer_arrival(connection)
+        serve_socket = connection[0]
+        incoming_peer = connection[1]
+        self.lg.info(f"{self.id}: accepted connection from peer {incoming_peer}")        
+
+    def send_public_endpoint(self, endpoint, peer_serve_socket):
+        self.lg.info(f"{self.id}: sending peer's (public) endpoint={endpoint}")
+        super().send_public_endpoint(endpoint = endpoint,
+                                     peer_serve_socket = peer_serve_socket)
+
+    def send_the_number_of_peers(self, peer_serve_socket):
+        self.lg.info(f"{self.id}: sending peers_list of length={len(self.team)}")
+        super().send_the_number_of_peers(peer_serve_socket)
+
+    def send_peer_index_in_team(self, peer_serve_socket, peer_index_in_team):
+        self.lg.info(f"{self.id}: sending peer_index_in_team={peer_index_in_team}")
+        super().send_peer_index_in_team(peer_serve_socket = peer_serve_socket,
+                                        peer_index_in_team = peer_index_in_team)
+
+    def insert_peer(self, peer):
+        super().insert_peer(peer)
+        self.lg.info(f"{self.id}: {peer} inserted in the team")
+        sys.stderr.write(f" {colorama.Fore.GREEN}{len(self.team)}{colorama.Style.RESET_ALL}"); sys.stderr.flush()
+        sys.stderr.write(f" {colorama.Fore.MAGENTA}{len(self.team)}{colorama.Style.RESET_ALL}"); sys.stderr.flush
+
+    def increment_unsupportivity_of_peer__warning(self, peer):
+        self.lg.warning(f"{self.id}: the unsupportive peer {peer} does not exist in {self.losses}")
+        
+    def increment_unsupportivity_of_peer(self, peer):
+        sys.stderr.write(f" {colorama.Fore.RED}({self.team.index(peer)}){colorama.Style.RESET_ALL}")
+        super().increment_unsupportivity_of_peer(peer)
+
+    def process_lost_chunk(self, lost_chunk_number):
+        super().process_lost_chunk(lost_chunk_number)
+        sys.stderr.write(f" {colorama.Fore.RED}{lost_chunk_number}{colorama.Style.RESET_ALL}")
+        #self.total_lost_chunks += 1
+
+    def del_peer(self, peer_index):
+        super().del_peer(peer_index)
+        sys.stderr.write(f" {colorama.Fore.BLUE}{peer_index}({len(self.team)}){colorama.Style.RESET_ALL}"); sys.stderr.flush()
+        sys.stderr.write(f" {colorama.Fore.MAGENTA}{len(self.team)}{colorama.Style.RESET_ALL}"); sys.stderr.flush
+
+    def remove_peer__warning1(self, peer):
+        self.lg.warning(f"{self.id}: the removed peer {peer} does not exist in {self.peer_list}")
+
+    def remove_peer__warning2(self, peer):
+        self.lg.warning(f"{self.id}: the removed peer {peer} does not exist in losses!")
+
+    def process_goodbye(self, peer):
+        self.lg.info(f"{self.id}: received [goodbye] from {peer}")
+        super().process_goodbye(peer)
+
+    def process_goodbye__feedback(self, peer):
+        self.lg.info(f"{self.id}: {peer} marked for deletion")
+
+    def remove_outgoing_peers__feedback(peer):
+        self.lg.info(f"{self.id}: outgoing peer {p}")
+        
+    def remove_outgoing_peers(self):
+        if __debug__:
+            if len(self.outgoing_peers_list) > 0:
+                sys.stderr.write(f"{self.id}: remove_outgoint_peers: len(outgoing_peers_list)={len(self.outgoing_peers_list)}\n")
+        super().remove_outgoing_peers()
+
+    def moderate_the_team__warning1(self, packed_msg, sender):
+        self.lg.warning(f"{self.id}: unexpected message {packed_msg} with length={len(packed_msg)} received from {sender}")
+
+    def moderate_the_team__warning2(self, msg, sender):
+        self.lg.info(f"{self.id}: received [lost chunk {msg[1]}] from {sender}")
+        
+    def moderate_the_team__warning3(self, packed_msg, msg, sender):
+        self.lg.warning(f"{self.id}: unexpected message {packed_msg} with length={len(packed_msg)} decoded as {msg} received from {sender}")
+
+    def moderate_the_team__hello_feedback(self, sender):
+        self.lg.info(f"{self.id}: received [hello] from {sender}")
+        
     def retrieve_chunk(self):
         # Simulator_stuff.LOCK.acquire(True,0.1)
         #time.sleep(Common.CHUNK_CADENCE)  # Simulates bit-rate control
@@ -44,6 +135,23 @@ class Splitter_DBS_simulator(Simulator_stuff, Splitter_DBS):
             #sys.stderr.write(str(len(self.team))); sys.stderr.flush()
         time.sleep(psutil.cpu_percent()/4000.0)
         return b'C'
+
+    def say_goodbye(self, peer):
+        super().say_goodbye(peer)
+        self.lg.info(f"{self.id}: sent [goodbye] to {peer}")
+
+    def run__waiting_feedback(self):
+        self.lg.info(f"{self.id}: waiting for a monitor peer")
+
+    def run__index_error_feedback(self):
+        self.lg.warning(f"{self.id}: the peer with index {self.peer_number} does not exist. peers_list={self.team} peer_number={self.peer_number}")
+
+    def run__destinations_feedback(self):
+        self.lg.debug(f"{self.id}: showing destination_of_chunk:\n")
+        counter = 0
+        for i in self.destination_of_chunk:
+            self.lg.debug(f"{counter} -> {i} ")
+            counter += 1
 
     def provide_feedback(self, peer_number, chunk_number):
         if Simulator_stuff.FEEDBACK:
@@ -60,32 +168,9 @@ class Splitter_DBS_simulator(Simulator_stuff, Splitter_DBS):
             #print("{}: len(peers_list)={}".format(self.id, len(self.team)))
             #sys.stderr.write(str(self.current_round) + "/" + str(self.max_number_of_rounds) + " " + str(self.chunk_number) + " " + str(len(self.team)) + "\r")
 
-    def process_lost_chunk(self, lost_chunk_number):
-        super().process_lost_chunk(lost_chunk_number)
-        sys.stderr.write(f" {colorama.Fore.RED}{lost_chunk_number}{colorama.Style.RESET_ALL}")
-        #self.total_lost_chunks += 1
-
-    def increment_unsupportivity_of_peer(self, peer):
-        sys.stderr.write(f" {colorama.Fore.RED}({self.team.index(peer)}){colorama.Style.RESET_ALL}")
-        super().increment_unsupportivity_of_peer(peer)
-#        sys.stderr.write(f"peer={peer}")
-
-    def insert_peer(self, peer):
-        super().insert_peer(peer)
-        sys.stderr.write(f" {colorama.Fore.GREEN}{len(self.team)}{colorama.Style.RESET_ALL}"); sys.stderr.flush()
-        sys.stderr.write(f" {colorama.Fore.MAGENTA}{len(self.team)}{colorama.Style.RESET_ALL}"); sys.stderr.flush
-
-    def del_peer(self, peer_index):
-        super().del_peer(peer_index)
-        sys.stderr.write(f" {colorama.Fore.BLUE}{peer_index}({len(self.team)}){colorama.Style.RESET_ALL}"); sys.stderr.flush()
-        sys.stderr.write(f" {colorama.Fore.MAGENTA}{len(self.team)}{colorama.Style.RESET_ALL}"); sys.stderr.flush
-        #        if __debug__:
-#            # S I M U L A T I O N
-#            if Simulator_stuff.FEEDBACK:
-#                Simulator_stuff.FEEDBACK["DRAW"].put(("O", "Node", "OUT", ','.join(map(str, peer))))
-#            if peer[0] == "M" and peer[1] != "P":
-#                self.number_of_monitors -= 1
-
+    def run__waiting_goodbyes_feedback(self):
+        self.lg.info("{}: waiting for [goodbye]s from peers (peers_list={})".format(self.id, self.team))
+            
     def run(self):
         super().run()
         sys.stderr.write("\n")
