187a3c08982ad72012b6562fed83af2574fa4861
Vicente Gonzalez Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-10-07 13:32:07 +0200

    .

--- experiments/CLR_vs_N_and_B_lat20/simulate.sh
+++ experiments/CLR_vs_N_and_B_lat20/simulate.sh
@@ -3,18 +3,19 @@
 # This generates a single sample for all the peers of the team
 
 STEP=1
-
+ROUNDS=100
 N=100 # Max team size
 while [ $N -gt 10 ]; do
     #echo N=$N
-    B=600 # Buffer size
+    B=1000 # Inial buffer size
     while [ $B -gt 20 ]; do
 	#echo B=$B
 	min_buff_size=`echo $N*2 | bc`
 	max_buff_size=`echo $N*3 | bc`
 	if [ $B -ge $min_buff_size ] && [ $B -le $max_buff_size ]; then
 	    echo $N $B
-	    time python3 ../../src/simulator.py run --loglevel INFO --number_of_peers $N --buffer_size $B --number_of_rounds 10 | grep "CLR" | awk '{match($0,/CLR=[0-9.]+/);A=substr($0,RSTART,RLENGTH);sub(/.*=/,X,A);print A}' >>  peers_${N}__buffer_size_${B}.txt
+	    echo python3 ../../src/simulator.py run --number_of_peers $N --buffer_size $B --number_of_rounds $ROUNDS
+	    time python3 ../../src/simulator.py run --number_of_peers $N --buffer_size $B --number_of_rounds $ROUNDS | grep "CLR" | awk '{match($0,/CLR=[0-9.]+/);A=substr($0,RSTART,RLENGTH);sub(/.*=/,X,A);print A}' >> peers_${N}__buffer_size_${B}.txt
 	    #python3 ../../src/simulator.py run --loglevel DEBUG --number_of_peers $N --buffer_size $B --number_of_rounds 100 | grep "CLR" 
 	fi
 	let B=B-$STEP
