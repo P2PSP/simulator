74f762bbe901364328262c5b89833a6fd9779ea1
Vicente Gonzalez Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-10-09 20:07:17 +0200

    .

--- experiments/CLR_vs_optimization/compute_averages.sh
+++ experiments/CLR_vs_optimization/compute_averages.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+SOR=$1
+
 printf "#B\tCLR\n"
 
 B=1000
@@ -7,7 +9,7 @@ N=100
 STEP=10
 while [ $B -ge 1 ]; do
     echo B=$B >&2
-    average=`python3 ../../tools/average.py < peers_${N}__buffer_size_${B}_DBS.txt | grep "average" | cut -d "=" -f 2`
+    average=`python3 ../average.py < peers_${N}__buffer_size_${B}__${SOR}.txt | grep "average" | cut -d "=" -f 2`
     printf $B
     printf "\t"
     printf $average
---
+++ experiments/CLR_vs_optimization/run.sh
@@ -0,0 +1,3 @@
+./simulate.sh
+./compute_averages.sh DBS > DBS.txt
+./compute_averages.sh DBS2 > DBS2.txt
