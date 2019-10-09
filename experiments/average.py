c0ba008606d9f3d705ab2b1970a765b0d6414a94
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-10-09 19:33:12 +0200

    .

---
+++ experiments/average.py
@@ -0,0 +1,21 @@
+###!/usr/bin/python3
+
+import sys
+
+counter = 0
+sum = 0.0
+for line in sys.stdin:
+    try:
+        number = float(line.strip())
+    except ValueError:
+        sys.stderr.write(f"ValueError exception: line={line}\n")
+        number = 0.0
+    #print(f"{number}")
+    sum += number
+    counter += 1
+try:
+    average = sum/counter
+except ZeroDivisionError:
+    average = 0.0
+
+sys.stdout.write(f"{average}\n")
