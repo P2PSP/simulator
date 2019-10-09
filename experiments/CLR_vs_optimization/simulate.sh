5a41c328e6f0252218644de5c0fcc0810d779339
Vicente Gonzalez Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-10-09 19:46:58 +0200

    .

--- experiments/CLR_vs_optimization/simulate.sh
+++ experiments/CLR_vs_optimization/simulate.sh
@@ -7,12 +7,22 @@ STEP=10
 
 B=1000
 while [ $B -gt 1 ]; do
-    time python3 ../../src/simulator.py run --set_of_rules DBS --number_of_peers $N --buffer_size $B --number_of_rounds $R | grep "CLR" | awk '{match($0,/CLR=[0-9.]+/);A=substr($0,RSTART,RLENGTH);sub(/.*=/,X,A);print A}' >> peers_${P}__buffer_size_${B}_DBS.txt
+    echo peers_${P}__buffer_size_${B}__DBS.txt
+    if [ -f peers_${P}__buffer_size_${B}__DBS.txt ]; then
+	echo exists
+    else
+	time python3 ../../src/simulator.py run --set_of_rules DBS --number_of_peers $N --buffer_size $B --number_of_rounds $R | grep "CLR" | awk '{match($0,/CLR=[0-9.]+/);A=substr($0,RSTART,RLENGTH);sub(/.*=/,X,A);print A}' >> peers_${P}__buffer_size_${B}__DBS.txt
+    fi
     let B=B-$STEP
 done
 
 B=1000
 while [ $B -gt 1 ]; do
-    time python3 ../../src/simulator.py run --set_of_rules DBS2 --number_of_peers $N --buffer_size $B --number_of_rounds $R | grep "CLR" | awk '{match($0,/CLR=[0-9.]+/);A=substr($0,RSTART,RLENGTH);sub(/.*=/,X,A);print A}' >> peers_${P}__buffer_size_${B}_DBS2.txt
+    echo peers_${P}__buffer_size_${B}__DBS2.txt
+    if [ -f peers_${P}__buffer_size_${B}__DBS2.txt ]; then
+	echo exists
+    else
+	time python3 ../../src/simulator.py run --set_of_rules DBS2 --number_of_peers $N --buffer_size $B --number_of_rounds $R | grep "CLR" | awk '{match($0,/CLR=[0-9.]+/);A=substr($0,RSTART,RLENGTH);sub(/.*=/,X,A);print A}' >> peers_${P}__buffer_size_${B}__DBS2.txt
+    fi
     let B=B-$STEP
 done
