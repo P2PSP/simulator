513be1722c926987b32699061c1b08bdcb791b61
Vicente Gonzalez Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-10-11 11:56:05 +0200

    .

--- experiments/DBS_vs_DBS2/simulate.sh
+++ experiments/DBS_vs_DBS2/simulate.sh
@@ -1,20 +1,22 @@
 #! /bin/bash
 
-N=99  # Monitor apart
+N=9  # Monitor apart
 let P=N+1
-R=200
-STEP=10
+R=100
+STEP=1
 
-B=200
-while [ $B -gt 1 ]; do
-    echo peers_${P}__buffer_size_${B}__DBS.txt
-    time python3 ../../src/simulator.py run --set_of_rules DBS --number_of_peers $N --buffer_size $B --number_of_rounds $R --max_chunk_loss_at_peers 10000 --max_chunk_loss_at_splitter 10000 | grep "CLR" | awk '{match($0,/CLR=[0-9.]+/);A=substr($0,RSTART,RLENGTH);sub(/.*=/,X,A);print A}' >> peers_${P}__buffer_size_${B}__DBS.txt
-    let B=B-$STEP
+B2=40
+B1=20
+while [ $B2 -gt 1 ]; do
+    echo peers_${P}__buffer_size_${B2}__DBS.txt
+    time python3 ../../src/simulator.py run --set_of_rules DBS --number_of_peers $N --buffer_size $B2 --number_of_rounds $R --max_chunk_loss_at_peers 10000 --max_chunk_loss_at_splitter 10000 | grep "CLR" | awk '{match($0,/CLR=[0-9.]+/);A=substr($0,RSTART,RLENGTH);sub(/.*=/,X,A);print A}' >> peers_${P}__buffer_size_${B2}__DBS.txt
+    let B2=B2-$STEP
 done
 
-B=200
-while [ $B -gt 1 ]; do
-    echo peers_${P}__buffer_size_${B}__DBS2.txt
-    time python3 ../../src/simulator.py run --set_of_rules DBS2 --number_of_peers $N --buffer_size $B --number_of_rounds $R --max_chunk_loss_at_peers 10000 --max_chunk_loss_at_splitter 10000 | grep "CLR" | awk '{match($0,/CLR=[0-9.]+/);A=substr($0,RSTART,RLENGTH);sub(/.*=/,X,A);print A}' >> peers_${P}__buffer_size_${B}__DBS2.txt
-    let B=B-$STEP
+B2=40
+B1=20
+while [ $B2 -gt $B1 ]; do
+    echo peers_${P}__buffer_size_${B2}__DBS2.txt
+    time python3 ../../src/simulator.py run --set_of_rules DBS2 --number_of_peers $N --buffer_size $B2 --number_of_rounds $R --max_chunk_loss_at_peers 10000 --max_chunk_loss_at_splitter 10000 | grep "CLR" | awk '{match($0,/CLR=[0-9.]+/);A=substr($0,RSTART,RLENGTH);sub(/.*=/,X,A);print A}' >> peers_${P}__buffer_size_${B}__DBS2.txt
+    let B2=B2-$STEP
 done
